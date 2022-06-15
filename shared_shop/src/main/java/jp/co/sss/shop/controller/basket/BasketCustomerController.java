f91a085f25f8817f1520d3b440fb2b67d946e775
hiraipika <hiraipika@gmail.com>
2022-06-15 09:09:28 +0900

    コミットをやり直します。

--- shared_shop/src/main/java/jp/co/sss/shop/controller/basket/BasketCustomerController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/basket/BasketCustomerController.java
@@ -28,131 +28,95 @@ public class BasketCustomerController {
 
 	@RequestMapping(path = "/basket/add", method = RequestMethod.POST)
 	public String addItem(Integer id, Model model) {
-
-		Integer listId = id-1;
+		System.out.println(id);
 		//買い物カゴに商品があるか確認。
 		//ない場合は「リスト」の作成。ある場合は、既存のリストの呼び出し。
 		if(session.getAttribute("orderItem")==null) {
 
-
 			List<BasketBean>items = new ArrayList<>();
+			
 			Item item = repository.getById(id);
 
 			//「BasketBean」に代入するための変数宣言
 
-			  String name = item.getName();
-			  Integer stock = item.getStock();
-			  Integer orderNum = 1;
-
+			String name = item.getName();
+			Integer stock = item.getStock();
+			Integer orderNum = 1;
 
 			//「BasketBean」に代入
 			BasketBean bean = new BasketBean(id,name,stock,orderNum);
+			
+			//リストに挿入する。
 			items.add(bean);
-
 
 			//スコープに挿入。
 			session.setAttribute("orderItem", items);
-			session.setAttribute("items", bean);
+			/*session.setAttribute("items", bean);
 			System.out.println(bean.getName());
 			System.out.println(bean.getStock());
 			System.out.println(bean.getOrderNum());
 			System.out.println(bean.getId());
-			System.out.println(items.get(0));
+			System.out.println(items.get(0));*/
 
 			return "basket/basket_shopping";
 
 
 		} else {
+			
 			//ある場合は、既存のリストを呼び出し。
 			List<BasketBean> items = (ArrayList)session.getAttribute("orderItem");
 
 
 			//アイテムが既にリストにあるか確認する。
-			//ある場合は上書き。ない場合は新たに作る。
-			if(items.get(listId)==null) {
-
-				Item item = repository.getById(id);
-
-
-				//「BasketBean」に代入するための変数宣言
-				String name = item.getName();
-				Integer stock = item.getStock();
-				Integer orderNum = 1;
-
-				//「BasketBean」に代入
-				BasketBean bean = new BasketBean(id,name,stock,orderNum);
-
-				//「List」に挿入。挿入位置を指定する。
-				items.add(bean);
+			//ある場合は上書き。ない場合は新たに作りリストに挿入。
+			items = elicitItemBean(items,id);
+			
 
 				//スコープに挿入。
 				session.setAttribute("orderItem", items);
-				session.setAttribute("items", bean);
 
 
 				return "basket/basket_shopping";
-
-			} else {
-
-				BasketBean bean = items.get(listId);
-
-				Item item = repository.getById(id);
-
-				//「BasketBean」に代入するための変数宣言
-
-				Integer newOrderNum = bean.getOrderNum()+1;
-
-				//「BasketBean」に代入
-				bean.setOrderNum(newOrderNum);
-
-				//「List」に挿入。挿入位置を指定する。
-				items.add(bean);
-
-				//スコープに挿入。
-				session.setAttribute("orderItem", items);
-				session.setAttribute("items", bean);
-
-
-				return "basket/basket_shopping";
-
-			}
 		}
 	}
 
 
-			public String addItem(Integer id) {
-				List<BasketBean>items = new ArrayList<>();
+
+	  public List<BasketBean> elicitItemBean (List<BasketBean>items,Integer id) {
+		  //リスト内での要素の位置を調べるための変数。
+			int number=0;
+			for(BasketBean bean : items) {
+			
+			//リストにカゴに入れたアイテムが存在した場合
+			if(bean.getId()==id) {
+				
+				bean.setOrderNum(bean.getOrderNum()+1);
+				
+				//位置を指定し「上書き」
+				items.set(number,bean);
+				
+				return items;
+	
+			} 
+			number++;
+		}
+				
 				Item item = repository.getById(id);
 
 				//「BasketBean」に代入するための変数宣言
-
 				String name = item.getName();
 				Integer stock = item.getStock();
 				Integer orderNum = 1;
 
 				//「BasketBean」に代入
-				BasketBean bean = new BasketBean(id,name,stock,orderNum);
-				//「List」に挿入。挿入位置を指定する。
-				items.add(bean);
-
-				session.setAttribute("orderItem", items);
-				session.setAttribute("items", item);
-				return "basket/basket_shopping";
-
-			}
+				BasketBean beans = new BasketBean(id,name,stock,orderNum);
+				
+				items.add(beans);
+				
+			return items;
+	  }
 }
 
-
-			/*public BasketBean elicitItemBean (List<BasketBean>items,String itemName) {
-				for(BasketBean bean : items) {
-					if(bean.getName()==itemName) {
-						return bean;
-					} else {
-					BasketBean bean = new BasketBean(id,name,stock,orderNum);
-					return bean;
-				}
-				}
-			}
 
 			/*@RequestMapping(path = "/basket/delete", method = RequestMethod.POST)
 			public String deleteItem(HttpSession session, BasketBean bean) {
