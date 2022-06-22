9a6e88728be0c35c2e2b7f527ada3a622697e313
yamada <hayato.yamada0701@gmail.com>
2022-06-22 10:38:22 +0900

    削除の際ログアウトするよう修正

--- shared_shop/src/main/java/jp/co/sss/shop/controller/user/UserDeleteCustomerController.java
+++ shared_shop/src/main/java/jp/co/sss/shop/controller/user/UserDeleteCustomerController.java
@@ -1,5 +1,7 @@
 package jp.co.sss.shop.controller.user;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -26,6 +28,9 @@ public class UserDeleteCustomerController {
 	 */
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	HttpSession session;
 	
 	/**
 	 * 会員情報削除確認処理
@@ -68,8 +73,11 @@ public class UserDeleteCustomerController {
 		
 		// 会員情報を保存
 		userRepository.save(user);
+		
+		//
+		session.removeAttribute("user");
 
-		return "redirect:/user/delete/complete";
+		return "user/delete/user_delete_complete";
 	}
 	
 	/**
@@ -80,7 +88,32 @@ public class UserDeleteCustomerController {
 	 */
 	@RequestMapping(path = "/user/delete/complete", method = RequestMethod.GET)
 	public String deleteCompleteRedirect() {
-
+		
 		return "user/delete/user_delete_complete";
+		
+	}
+	
+	/**
+	 * 会員情報 削除確認処理から会員詳細画面に戻る処理
+	 *
+	 * @param model Viewとの値受渡し
+	 * @param form  会員情報フォーム
+	 * @return "/user/detail/{id}" 会員情報 詳細画面へ
+	 */
+	@RequestMapping(path = "/user/delete/check", method = RequestMethod.GET)
+	public String deleteBack(Model model, @ModelAttribute UserForm form) {
+
+		// 削除対象の会員情報を取得
+		User user = userRepository.getById(form.getId());
+
+		UserBean userBean = new UserBean();
+
+		// Userエンティティの各フィールドの値をUserBeanにコピー
+		BeanUtils.copyProperties(user, userBean);
+
+		// 会員情報をViewに渡す
+		model.addAttribute("user", userBean);
+
+		return "user/detail/{id}";
 	}
 }
